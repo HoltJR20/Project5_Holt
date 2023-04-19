7e060297858e4552ae29e046a2cb07f075c1c9eb
JAKE2119 <HoltJR20@gcc.edu>
2023-04-19 19:47:04 -0400

    javadoc for the GUI class

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,40 @@
+/**
+ * A class to create a GUI for the game of checkers
+ */
 public class GUI {
+    /**
+     * this method views the board
+     */
+    public void BoardView(){
 
+    }
+
+    /**
+     * this method views individual squares on the board
+     *
+     */
+    public void SquareView(){
+
+    }
+
+    /**
+     * This method vies the pieces and allows access to movement
+     */
+    public void Piece(){
+
+    }
+
+    /**
+     * this method decides if the player is human
+     */
+    public void HumanPlayer(){
+
+    }
+
+    /**
+     * this method decides if the player is a computer
+     */
+    public void ComputerPlayer(){
+
+    }
 }
