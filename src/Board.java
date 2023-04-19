dcff872d45663a44272172d137e747d685a0e714
COMSTOCKKP22 <comstockkp22@gcc.edu>
2023-04-19 18:59:20 -0400

    New classes added

---
+++ src/Board.java
@@ -0,0 +1,20 @@
+public class Board {
+
+    private final int SIZE = 8;
+
+    private char[][] board = new char[SIZE][SIZE];
+
+    private Player currentPlayer;
+
+    private Player whitePlayer;
+
+    private Player blackPlayer;
+
+    public Piece getPiece(int row, int col) {
+
+    }
+
+    public void movePiece(Player player, int fromRow, int fromCol, int toRow, int toCol){
+
+    }
+}
---
+++ src/Checkers.java
@@ -0,0 +1,18 @@
+public class Checkers {
+
+    public Checkers() {
+
+    }
+
+    public void play() {
+
+    }
+
+    public boolean isEndGame() {
+
+    }
+
+    public Player getWinner() {
+
+    }
+}
