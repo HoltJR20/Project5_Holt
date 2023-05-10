1afbea38babb6144cb7f5fcd64910c57d8f17c0f
JAKE2119 <HoltJR20@gcc.edu>
2023-05-10 17:49:50 -0400

    Updated Checker play method to take player input and wrote a main class

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+       Checkers checkers = new Checkers();
 
-            // create a new game board
-            Board board = new Board();
-
-
-
-            // print the initial state of the board
-            System.out.println(board);
-
-
-            // print the updated state of the board
-            System.out.println(board);
-        }
+        checkers.play();
     }
 
 }
+
+
+
